dc4fea61ea38dee1fcd94fad2f9765bfd7afb76a
Hersfold <hersfoldwiki@gmail.com>
2011-10-22 23:09:34 +0000

    now we're looking better :-)

--- public_html/dev/index.php
+++ public_html/dev/index.php
@@ -7,20 +7,29 @@
 </head>
 <body>
 <div id="header">
-Unblock<br />
-Ticket<br />
-Request<br />
-System<br />
+English Wikipedia<br />
+Unblock Ticket Request System
 </div>
 <div id="subheader">
+<table class="subheader_content">
+<tr>
+<td>
 <a id="GAB" href="http://en.wikipedia.org/wiki/Wikipedia:Guide_to_appealing_blocks">Guide to Appealing Blocks</a>
+</td>
+<td>
 <a id="loginLink" href="login.php">Admins: Log in to review requests</a>
+</td>
+<td>
+<a id="loginLink" href="register.php">Admins: Request an account</a>
+</td>
+</tr>
+</table>
 </div>
 <div id="main">
 <center><b>Welcome to the Unblock Ticket Request System.</b></center>
 
 <p>If you are presently blocked from editing on Wikipedia (which you may verify by 
-clicking <a href="http://en.wikipedia.org/w/index.php?title=Wikipedia:Sandbox?action=edit">here</a>, you may fill out
+clicking <a href="http://en.wikipedia.org/w/index.php?title=Wikipedia:Sandbox?action=edit">here</a>), you may fill out
 the form below to have an administrator review your block. Please complete all fields labelled in 
 <span class="required">red text</span>, as these are required in order for us to complete a full review of your block.</p>
 
@@ -33,28 +42,24 @@ how to receive assistance, please see those links.</p>
 <p><b>For assistance with a block, please complete the form below:</b></p>
 
 <form name="unblockAppeal" id="unblockAppeal" action="index.php" method="POST">
-<label id="accountYNLabel" for="registered" class="required">Do you have an account on Wikipedia?</label> Yes <input type="radio" name="registered" value="true" /> No <input type="radio" name="registered" value="false" /><br />
+<label id="emailLabel" for="accountName" class="required">What is your email address? We will need this to respond to your appeal.</label> <input type="text" name="email" value=""/><br />
+<label id="registeredLabel" for="registered" class="required">Do you have an account on Wikipedia?</label> &#09; <input type="radio" name="registered" value="true" /> Yes &#09; <input type="radio" name="registered" value="false" /> No<br />
 <label id="accountNameLabel" for="accountName" class="required">What is the name of your account?</label> <input type="text" name="accountName" value=""/><br />
-<label id="accountYNLabel" for="autoBlock" class="required">What has been blocked?</label> My account <input type="radio" name="autoBlock" value="false" /> My IP address (my account is not blocked) <input type="radio" name="autoBlock" value="true" /><br />
+<label id="autoBlockLabel" for="autoBlock" class="required">What has been blocked?</label> &#09; <input type="radio" name="autoBlock" value="false" /> My account &#09; <input type="radio" name="autoBlock" value="true" /> My IP address (my account is not blocked)<br />
 <label id="appealLabel" for="appeal" class="required">Why do you believe you should be unblocked?</label><br />
 <textarea name="appeal" rows="5" cols="50"> </textarea><br />
 <label id="editsLabel" for="edits" class="required">If you are unblocked, what articles to you intend to edit?</label><br />
 <textarea name="edits" rows="5" cols="50"> </textarea><br />
-<label id="otherInfoLabel" for="otherInfo">is there anything else you would like us to consider when reviewing your block?</label><br />
+<label id="otherInfoLabel" for="otherInfo">Is there anything else you would like us to consider when reviewing your block?</label><br />
 <textarea name="otherInfo" rows="3" cols="50"> </textarea><br />
-<label id="desiredAccountNameLabel" for="desiredAccountName">We may be able to create an account for you which you can use to avoid problems like this in the future. <br />
-If you would like for us to make an account for you, please enter the username you'd like to use here.</label> <input type="text" name="desiredAccountName" value=""/><br />
+<label id="desiredAccountNameLabel" for="desiredAccountName">We may be able to create an account for you which you can use to avoid problems like this in the future. If you would like for us to make an account for you, please enter the username you'd like to use here.</label><br/><input type="text" name="desiredAccountName" value=""/><br />
 
 CAPTCHA GOES HERE
 
 <input type="submit" value="Submit Appeal"/>
 </form>
 
-<p>Please remember that Wikipedia adminstrators are volunteers; it may take some time for your appeal to be reviewed, <br />
-and a courteous appeal will meet with a courteous response. If you feel it is taking too long for your appeal to be reviewed, <br/>
-you can usually appeal your block on your user talk page (<a href="http://en.wikipedia.org/wiki/Special:Mytalk">located here</a>)<br />
-by copying this text and pasting it in a new section on the bottom of your page. Be sure to replace "your reason here"<br />
-with your appeal: <b><tt>{{unblock|1=your request here}}</tt></b></p>
+<p>Please remember that Wikipedia adminstrators are volunteers; it may take some time for your appeal to be reviewed, and a courteous appeal will meet with a courteous response. If you feel it is taking too long for your appeal to be reviewed, you can usually appeal your block on your user talk page (<a href="http://en.wikipedia.org/wiki/Special:Mytalk">located here</a>) by copying this text and pasting it in a new section on the bottom of your page. Be sure to replace "your reason here" with your appeal: <b><tt>{{unblock|1=your reason here}}</tt></b></p>
 </div>
 <div id="footer">
 The Unblock Ticket Request System is a project hosted on the Wikimedia Toolserver intended to assist

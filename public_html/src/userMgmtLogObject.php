434cc5f5a5332a75684cec44dfd316ade084dde2
Hersfold <hersfoldwiki@gmail.com>
2012-01-23 16:44:59 +0000

    and again

--- public_html/src/userMgmtLogObject.php
+++ public_html/src/userMgmtLogObject.php
@@ -2,9 +2,9 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
-require_once('../src/exceptions.php');
-require_once('../src/unblocklib.php');
-require_once('../src/userObject.php');
+require_once('exceptions.php');
+require_once('unblocklib.php');
+require_once('userObject.php');
 
 
 class UserMgmtLog{

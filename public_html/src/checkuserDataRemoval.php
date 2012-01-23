688cdc59d73248f9c7173a475c6a34e430cba4c9
Hersfold <hersfoldwiki@gmail.com>
2012-01-23 16:40:51 +0000

    adding script to be run as a cron job to remove private data from closed requests

---
+++ public_html/src/checkuserDataRemoval.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * This script is intended to be run from the command line, ideally
+ * with a cron job, on a weekly basis. It will search the database for 
+ * any closed appeals and delete their associated privacy-related data.
+ */
+
+require_once('unblocklib.php');
+require_once('exceptions.php');
+
+echo 'Starting to clear out private data from closed appeals.\n';
+
+try{
+
+	$db = connectToDB();
+
+	$query = "SELECT appealID FROM appeal WHERE email IS NOT NULL AND status = 'CLOSED'";
+
+	echo 'Running query: ' . $query . '\n';
+
+	$result = mysql_query($query, $db);
+
+	if(!$result){
+		throw new UTRSDatabaseException(mysql_error($db));
+	}
+	$rows = mysql_num_rows($result);
+	if($rows == 0){
+		echo 'There are no recently closed appeals that need data removed.\n';
+	}
+	else{
+
+		echo 'Getting appeal IDs from ' . $rows . ' appeals...\n';
+
+		$appealIds = array();
+
+		for ($i = 0; $i < $rows; $i++){
+			$row = mysql_fetch_assoc($result);
+			$appealIds[$i] = $row['appealID'];
+		}
+
+		echo 'Starting to remove private data...\n';
+
+		for($i = 0; $i < $rows; $i++){
+			$appeal = $appealIds[$i];
+			echo 'Processing appeal #' . $appeal . '\n';
+			$query = "UPDATE appeal SET email = NULL, ip = NULL WHERE appealID = '" . $appeal . "'";
+			echo '\tRunning query: ' . $query . '\n';
+			$result = mysql_query($query, $db);
+			if(!$result){
+				throw new UTRSDatabaseException(mysql_error($db));
+			}
+			// else
+			$query = "DELETE FROM cuData WHERE appealID = '" . $appeal . "'";
+			echo '\tRunning query: ' . $query . '\n';
+			$result = mysql_query($query, $db);
+			if(!$result){
+				throw new UTRSDatabaseException(mysql_error($db));
+			}
+			echo 'Appeal #' . $appeal . ' complete.\n';
+		}
+	}
+
+	echo 'Script completed successfully!\n';
+
+}
+catch(Exception $e){
+	echo 'ERROR - Exception thrown!\n';
+	echo $e->getMessage() . '\n';
+	echo 'Script incomplete.\n';
+}
+
+exit;
+?>

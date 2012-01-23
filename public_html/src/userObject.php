c8740d57407742bf1b01449241a53ea5a0467a2b
Hersfold <hersfoldwiki@gmail.com>
2012-01-23 16:44:17 +0000

    removing relative import paths

--- public_html/src/userObject.php
+++ public_html/src/userObject.php
@@ -2,9 +2,9 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
-require_once('../src/exceptions.php');
-require_once('../src/unblocklib.php');
-require_once('../src/userMgmtLogObject.php');
+require_once('exceptions.php');
+require_once('unblocklib.php');
+require_once('userMgmtLogObject.php');
 
 
 class User{

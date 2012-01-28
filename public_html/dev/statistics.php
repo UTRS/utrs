2b6c6af604b5228472002db9ed8383f5e2042cd4
DeltaQuad <deltaquadwiki@gmail.com>
2012-01-28 23:33:27 +0000

    Add all number of users stats

--- public_html/dev/statistics.php
+++ public_html/dev/statistics.php
@@ -28,7 +28,7 @@ function getNumUsers() {
 	
 	$query = "SELECT * FROM `user` WHERE `username` LIKE '%'";
 	mysql_query($query);
-	echo "Number of tool users: %d\n", mysql_affected_rows();
+	echo "Number of tool users: ", mysql_affected_rows();
 	
 	debug($query . '<br/>');
 	
@@ -42,7 +42,117 @@ function getNumUsers() {
 	debug('complete <br/>');
 }
 
+function getNumApproved() {
+	debug('Query number of users <br />');
+
+	$db = connectToDB();
+
+	$query = "SELECT *  FROM `user` WHERE `approved` = 1";
+	mysql_query($query);
+	echo "Number of tool users approved: ", mysql_affected_rows();
+
+	debug($query . '<br/>');
+
+	$result = mysql_query($query, $db);
+	if(!$result){
+		$error = mysql_error($db);
+		debug('ERROR: ' . $error . '<br/>');
+		throw new UTRSDatabaseException($error);
+	}
+
+	debug('complete <br/>');
+}
+
+function getNumActive() {
+	debug('Query number of users <br />');
+
+	$db = connectToDB();
+
+	$query = "SELECT *  FROM `user` WHERE `active` = 1";
+	mysql_query($query);
+	echo "Number of tool users active: ", mysql_affected_rows();
+
+	debug($query . '<br/>');
+
+	$result = mysql_query($query, $db);
+	if(!$result){
+		$error = mysql_error($db);
+		debug('ERROR: ' . $error . '<br/>');
+		throw new UTRSDatabaseException($error);
+	}
+
+	debug('complete <br/>');
+}
+
+function getNumAdmins() {
+	debug('Query number of users <br />');
+
+	$db = connectToDB();
+
+	$query = "SELECT *  FROM `user` WHERE `toolAdmin` = 1";
+	mysql_query($query);
+	echo "Number of tool administrators: ", mysql_affected_rows();
+
+	debug($query . '<br/>');
+
+	$result = mysql_query($query, $db);
+	if(!$result){
+		$error = mysql_error($db);
+		debug('ERROR: ' . $error . '<br/>');
+		throw new UTRSDatabaseException($error);
+	}
+
+	debug('complete <br/>');
+}
+
+function getNumCU() {
+	debug('Query number of users <br />');
+
+	$db = connectToDB();
+
+	$query = "SELECT *  FROM `user` WHERE `checkuser` = 1";
+	mysql_query($query);
+	echo "Number of checkusers: ", mysql_affected_rows();
+
+	debug($query . '<br/>');
+
+	$result = mysql_query($query, $db);
+	if(!$result){
+		$error = mysql_error($db);
+		debug('ERROR: ' . $error . '<br/>');
+		throw new UTRSDatabaseException($error);
+	}
+
+	debug('complete <br/>');
+}
+
+function getNumDevs() {
+	debug('Query number of users <br />');
+
+	$db = connectToDB();
+
+	$query = "SELECT *  FROM `user` WHERE `developer` = 1";
+	mysql_query($query);
+	echo "Number of tool developers: ", mysql_affected_rows();
+
+	debug($query . '<br/>');
+
+	$result = mysql_query($query, $db);
+	if(!$result){
+		$error = mysql_error($db);
+		debug('ERROR: ' . $error . '<br/>');
+		throw new UTRSDatabaseException($error);
+	}
+
+	debug('complete <br/>');
+}
+
 getNumUsers();
+getNumApproved();
+getNumActive();
+getNumAdmins();
+getNumCU();
+getNumDevs();
 
 skinFooter();
 

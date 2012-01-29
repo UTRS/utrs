142d18c27ab0f6847d07c45f14ab754201c74eff
TParis <tparis@ufpfleet.org>
2012-01-29 17:29:46 +0000

    Create comments page

---
+++ public_html/dev/comment.php
@@ -0,0 +1,52 @@
+<?php
+//Created by the unblock-en-l dev team (test commit)
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+ini_set('session.use_cookies', '1');
+
+require_once('../src/unblocklib.php');
+require_once('../src/exceptions.php');
+require_once('../src/appealObject.php');
+require_once('../src/userObject.php');
+require_once('../src/templateObj.php');
+require_once('../src/logObject.php');
+require_once('template.php');
+
+// make sure user is logged in, if not, kick them out
+verifyLogin('appeal.php?id=' . $_GET['id']);
+
+$errorMessages = '';
+
+//Template header()
+skinHeader();
+
+if (!is_numeric($_GET['id'])) {
+	throw new UTRSIllegalModificationException('Appeal id is not numeric.');
+}
+
+//construct appeal object
+$appeal = Appeal::getAppealByID($_GET['id']);
+
+?>
+<form action="appeal.php?id=<?php echo $_GET['id']; ?>&action=comment" method="POST">
+<table>
+	<tr>
+		<th>Appeal</th>
+		<td><?php echo $appeal->getCommonName(); ?></td>
+	</tr>
+	<tr>
+		<td colspan="2">
+			<textarea name="comment" rows="15" cols="30"></textarea>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="2">
+			<input type="submit" value="Submit comment">
+		</td>
+	</tr>
+</table>
+</form>
+<?php 
+
+skinFooter();
+?>

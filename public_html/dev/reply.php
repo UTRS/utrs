01ff61edf1e29254f8e52c12ff68b09dbedd8b34
Hersfold <hersfoldwiki@gmail.com>
2012-01-30 02:45:20 +0000

    fixing logic

--- public_html/dev/reply.php
+++ public_html/dev/reply.php
@@ -34,7 +34,7 @@ try{
 		   "arbcom-l AT lists DOT wikimedia DOT org.");
 		   
 	}
-	if(!isset($_GET['confirmEmail']) | strcmp($_GET['confirmEmail'], $appeal->getEmail()) !== 0){
+	if(!isset($_GET['confirmEmail']) || strcmp($_GET['confirmEmail'], $appeal->getEmail()) !== 0){
 		throw new UTRSIllegalModificationException("Please use the link provided to you in your email to access this page. " .
 		   "This security step assures us that we are still talking to the same person. Thank you.");
 	}
@@ -67,12 +67,12 @@ if(!$accessGranted){
 	displayError($errors);
 }
 else{
-	echo "Submitted: " . $submitted . " Errors: " . $errors . " Not sub OR errors: " . (!$submitted | $errors) . "\n\n";
+	echo "Submitted: " . $submitted . " Errors: " . $errors . " Not sub OR errors: " . (!$submitted || $errors) . "\n\n";
 ?>
 
 <h3>Post a reply to your appeal</h3>
 
-<?php if(!$submitted | $errors ){ ?>
+<?php if(!$submitted || $errors ){ ?>
 <p>Welcome back, <?php echo $appeal->getCommonName(); ?>. You may use the form below to respond to any emails
 you have received from the administrator(s) reviewing your block. Posting a response with this form will flag
 your appeal for further attention from whoever is currently reviewing it, so if you believe your appeal has 
@@ -80,14 +80,14 @@ been heavily delayed, you can post a response to bring attention to it again.</p
 
 <?php } // closes if(!$submitted | $errors )
 
-if($submitted & !$errors){
+if($submitted && !$errors){
 	displaySuccess("Your response has been successfully posted. We will be in touch with you again soon.");
 }
 else if($errors){
 	displayError($errors);
 }
 
-if(!$submitted | $errors ){
+if(!$submitted || $errors ){
 ?>
 <form name="sendReply" id="sendReply" method="POST" action="reply.php?id=<?php echo $_GET['id'];?>&confirmEmail=<?php echo $_GET['confirmEmail'];?>">
 <textarea rows="15" cols="60" name="reply" id="reply"><?php if(isset($_POST['reply'])){echo $_POST['reply'];}?></textarea>

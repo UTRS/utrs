5fde8df3520eeff51f6a8a60d5c532b1b24e8f60
Hersfold <hersfoldwiki@gmail.com>
2012-01-30 02:18:46 +0000

    complete reply form

--- public_html/dev/reply.php
+++ public_html/dev/reply.php
@@ -13,6 +13,8 @@ require_once('../src/logObject.php');
 require_once('template.php');
 
 $errors = '';
+$appeal = null;
+$accessGranted = false;
 
 if(!isset($_GET['id'])){
 	// if not supposed to be here, send to appeals page
@@ -35,15 +37,64 @@ try{
 		throw new UTRSIllegalModificationException("Please use the link provided to you in your email to access this page. " .
 		   "This security step assures us that we are still talking to the same person. Thank you.");
 	}
+	
+	$accessGranted = true;
 
 	if(isset($_POST['submit'])){
 		// stuff goes here
 		$log = Log::getCommentsByAppealId($appeal->getID());
 		
-		// more stuff
-	}
+		$reply = $_POST['reply'];
+		if(strlen($reply) === 0){
+			throw new UTRSIllegalModificationException("You may not post a blank reply.");
+		}
 		
+		$log->addNewItem($reply);
+		
+		$appeal->setStatus(Appeal::$STATUS_AWAITING_REVIEWER);
+		$appeal->update();
+	}
 }
 catch(UTRSException $e){
 	$errors = $e->getMessage();
 }
+
+skinHeader();
+
+if(!$accessGranted){
+	displayError($errors);
+}
+else{
+?>
+
+<h3>Post a reply to your appeal</h3>
+
+<?php if(!isset($_POST['submit']) | $errors ){ ?>
+<p>Welcome back, <?php echo $appeal->getCommonName(); ?>. You may use the form below to respond to any emails
+you have received from the administrator(s) reviewing your block. Posting a response with this form will flag
+your appeal for further attention from whoever is currently reviewing it, so if you believe your appeal has 
+been heavily delayed, you can post a response to bring attention to it again.</p>
+
+<?php } // closes if(!isset($_POST['submit']) | $errors )
+
+if(isset($_POST['submit']) & !$errors){
+	displaySuccess("Your response has been successfully posted. We will be in touch with you again soon.");
+}
+else if($errors){
+	displayError($errors);
+}
+
+if(!isset($_POST['submit']) | $errors ){
+?>
+<form name="sendReply" id="sendReply" method="POST" action="reply.php?id=<?php echo $_GET['id'];?>&confirmEmail=<?php echo $_GET['confirmEmail'];?>">
+<textarea rows="15" cols="60" name="reply" id="reply"><?php if(isset($_POST['reply'])){echo $_POST['reply'];}?></textarea>
+<input name="submit" id="submit" type="submit" value="Send Reply" />
+</form>
+<?php 
+} // closes if(!isset($_POST['submit']) | $errors )
+
+} // closes else from if(!$accessGranted)
+
+skinFooter();
+
+?>

d3d52fa2b8a9e040465086b152ae251074adfc73
Hersfold <hersfoldwiki@gmail.com>
2012-01-30 03:22:18 +0000

    fix closed error

--- public_html/dev/reply.php
+++ public_html/dev/reply.php
@@ -28,7 +28,7 @@ try{
 		throw new UTRSIllegalModificationException("Your appeal has been marked as closed, which means the adminstrator" .
 		   " reviewing your appeal feels the matter is resolved. If you received a message that indicates you will be " .
 		   "unblocked, but you still cannot edit, please try again in a few minutes. If you are still unable to edit," .
-		   " you may wish to post <tt>{{unblock|<contents of your email here>}} to your " .
+		   " you may wish to post <tt>{{unblock|&lt;contents of your email here&gt;}}</tt> to your " .
 		   "<a href=\"http://enwp.org/Special:Mytalk\">User Talk: page</a>. If you appeal was declined, then you may " .
 		   "wish to appeal again in several month's time, or appeal to the Ban Appeals Subcommittee by emailing " .
 		   "arbcom-l AT lists DOT wikimedia DOT org.");

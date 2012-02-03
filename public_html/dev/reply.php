3153bfd2da1f36e55776d2368f73835035137389
Hersfold <hersfoldwiki@gmail.com>
2012-02-03 01:22:42 +0000

    switching order of checks on reply page

--- public_html/dev/reply.php
+++ public_html/dev/reply.php
@@ -24,6 +24,11 @@ if(!isset($_GET['id'])){
 try{
 	$id = $_GET['id'];
 	$appeal = Appeal::getAppealByID($id);
+
+	if(!isset($_GET['confirmEmail']) || strcmp($_GET['confirmEmail'], $appeal->getEmail()) !== 0){
+		throw new UTRSIllegalModificationException("Please use the link provided to you in your email to access this page. " .
+		   "This security step assures us that we are still talking to the same person. Thank you.");
+	}
 	if(strcmp($appeal->getStatus(), Appeal::$STATUS_CLOSED) === 0){
 		throw new UTRSIllegalModificationException("Your appeal has been marked as closed, which means the adminstrator" .
 		   " reviewing your appeal feels the matter is resolved. If you received a message that indicates you will be " .
@@ -33,10 +38,6 @@ try{
 		   "wish to appeal again in several month's time, or appeal to the Ban Appeals Subcommittee by emailing " .
 		   "arbcom-l AT lists DOT wikimedia DOT org.");
 		   
-	}
-	if(!isset($_GET['confirmEmail']) || strcmp($_GET['confirmEmail'], $appeal->getEmail()) !== 0){
-		throw new UTRSIllegalModificationException("Please use the link provided to you in your email to access this page. " .
-		   "This security step assures us that we are still talking to the same person. Thank you.");
 	}
 	
 	$accessGranted = true;

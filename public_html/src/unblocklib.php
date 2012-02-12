7f5b8fb82911eba38f7a4e94d7495f2a0367ff03
Hersfold <hersfoldwiki@gmail.com>
2012-02-12 19:15:45 +0000

    UTRS-53: implement new email function

--- public_html/src/unblocklib.php
+++ public_html/src/unblocklib.php
@@ -263,66 +263,138 @@ function getCurrentUser(){
  */
 function validEmail($email)
 {
-	$isValid = true;
-	$atIndex = strrpos($email, "@");
-	if (is_bool($atIndex) && !$atIndex)
-	{
-		$isValid = false;
+	// if does not contain an @ or @ is first character
+	if(strpos($email, "@") === false || strpos($email, "@") === 0){
+		return false;
 	}
-	else
-	{
-		$domain = substr($email, $atIndex+1);
-		$local = substr($email, 0, $atIndex);
-		$localLen = strlen($local);
-		$domainLen = strlen($domain);
-		if ($localLen < 1 || $localLen > 64)
-		{
-			// local part length exceeded
-			$isValid = false;
+	// get the domain and user parts, assumed to be separated
+	// at the last @ in the email addy
+	$user = substr($email, 0, strrpos($email, "@"));
+	$domain = substr($email, strrpos($email, "@") + 1);
+	// validate user side
+	$userArray = str_split($user, 1);
+	$length = sizeof($userArray);
+	// local part may only be 64 characters long
+	if($length > 64){
+		return false;
+	}
+	$inQuotes = false;
+	$inComment = false;
+	$escapeNext = false;
+	// this is a somewhat slow way of doing things, but avoids potential
+	// for errors.
+	for($i = 0; $i < $length; $i++){
+		$char = $userArray[$i];
+		// normal stuff
+		if(preg_match("/^[a-zA-Z0-9!#$%&'*+\-\/_=?^+`{}|~]$/", $char)){
+			$escapeNext = false; // don't need to escape these, but don't see why you can't
+			continue; // nothing to worry about
 		}
-		else if ($domainLen < 1 || $domainLen > 255)
-		{
-			// domain part length exceeded
-			$isValid = false;
+		// special characters not including ., (, ), ", \
+		if(preg_match("/^[ ,:;<>@\[\]]$/", $char)){
+			if($escapeNext){
+				$escapeNext = false;
+				continue; // character properly escaped, move on
+			}
+			if($inQuotes || $inComment){
+				continue; // nobody cares, move on
+			}
+			return false; // illegal character
 		}
-		else if ($local[0] == '.' || $local[$localLen-1] == '.')
-		{
-			// local part starts or ends with '.'
-			$isValid = false;
+		// dot
+		if(preg_match("/^[.]$/", $char)){
+			if($inQuotes || $inComment){
+				continue; // nobody cares, move on
+			}
+			// if first, last, or next character is also a dot
+			if($i == 0 || $i == ($length - 1) || preg_match("/^[.]$/", $userArray[$i+1])){
+				return false;
+			}
+			$escapeNext = false;
+			continue; // otherwise we don't care
 		}
-		else if (preg_match('/\\.\\./', $local))
-		{
-			// local part has two consecutive dots
-			$isValid = false;
+		// quote
+		if(preg_match("/^[\"]$/", $char)){
+			if($inComment){
+				echo "In comment\n";
+				continue; // nobody cares, move on
+			}
+			if(!$inQuotes){
+				// if first or previous character is a dot
+				if($i == 0 || preg_match("/^[.]$/", $userArray[$i-1])){
+					$inQuotes = true;
+					continue; // start of valid quoted string, carry on
+				}
+			}
+			else{
+				// if last or next character is a dot
+				if($i == ($length - 1) || preg_match("/^[.]$/", $userArray[$i+1])){
+					$inQuotes = false;
+					continue; // end of valid quoted string, carry on
+				}
+			}
+			if($escapeNext){
+				$escapeNext = false;
+				continue; // escaped, carry on
+			}
+			return false; // otherwise invalid character
 		}
-		else if (!preg_match('/^[A-Za-z0-9\\-\\.]+$/', $domain))
-		{
-			// character not valid in domain part
-			$isValid = false;
-		}
-		else if (preg_match('/\\.\\./', $domain))
-		{
-			// domain part has two consecutive dots
-			$isValid = false;
-		}
-		else if (!preg_match('/^(\\\\.|[A-Za-z0-9!#%&`_=\\/$\'*+?^{}|~.-])+$/',
-		str_replace("\\\\","",$local)))
-		{
-			// character not valid in local part unless
-			// local part is quoted
-			if (!preg_match('/^"(\\\\"|[^"])+"$/',
-			str_replace("\\\\","",$local)))
-			{
-				$isValid = false;
+		// backslash
+		if(preg_match("/^[\\\\]$/", $char)){
+			if($escapeNext){
+				$escapeNext = false;
+				continue; // escaped, carry on
+			}
+			// if last
+			else if($i == ($length - 1)){
+				return false; // can't be last, as that escapes the @, 
+				              // making the address not actually have an @ 
+			}
+			else{
+				$escapeNext = true;
+				continue; // escape whoever's next, carry on
 			}
 		}
-		if ($isValid && !(checkdnsrr($domain,"MX") || checkdnsrr($domain,"A")))
-		{
-			// domain not found in DNS
-			$isValid = false;
+		// open paren
+		if(preg_match("/^[(]$/", $char)){
+			if($escapeNext){
+				return false; // not a valid character by itself
+			}
+			$inComment = true;
+			continue; // keep going
 		}
+		// close paren
+		if(preg_match("/^[)]$/", $char)){
+			if($inComment){
+				if($escapeNext){
+					$escapeNext = false;
+					continue; // escaped, carry on
+				}
+				$inComment = false;
+				continue;
+			}
+			return false; // otherwise invalid character
+		}
+		return false; // if not told to continue by now, the character is invalid
 	}
-	return $isValid;
+	if($inQuotes || $inComment || $escapeNext){
+		return false;
+	}
+	// end user validation
+	// begin domain validation
+	// remove comments
+	$domain = preg_replace("/\(.*?\)/", "", $domain);
+	// IP address wrapped in [] (e.g. [127.0.0.1])
+	if(!(preg_match("/^\[((2[0-4][0-9]|25[0-5]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.){3,3}".
+	   "(2[0-4][0-9]|25[0-5]|1[0-9][0-9]|[1-9][0-9]|[0-9])\]$/", $domain)) && 
+		!(preg_match("/^[a-zA-Z0-9-]+?(\.[a-zA-Z0-9-]+?){0,}$/", $domain))){
+		return false;
+	}
+	else if(strlen($domain) > 255){
+		return false;
+	}
+	
+	return true; // yay!
 }
 
 /**

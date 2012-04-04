92d2d859260d705a4d3c47a5326fb1a3856bad1c
TParis <tparis@ufpfleet.org>
2012-04-04 21:59:05 +0000

    Fixed an error in the way the backlog reads closed appeals by only counting a "lastLogID" if it is an action rather than a comment.

--- public_html/src/logObject.php
+++ public_html/src/logObject.php
@@ -9,7 +9,7 @@ require_once('unblocklib.php');
 
 /**
  * This class contains information relevant to a single unblock appeal.
- * 
+ *
  */
 class LogItem {
 	private $commentID;
@@ -18,7 +18,7 @@ class LogItem {
 	private $comment;
 	private $commentUser;
 	private $action;
-	
+
 	public function __construct($vars) {
 		$this->commentID = $vars['commentID'];
 		$this->appealID = $vars['appealID'];
@@ -27,22 +27,22 @@ class LogItem {
 		$this->commentUser = $vars['commentUser'];
 		$this->action = $vars['action'];
 	}
-	
+
 	function getLogArray() {
 		return array('commentID' => $this->commentID, 'appealID' => $this->appealID, 'timestamp' => $this->timestamp, 'comment' => $this->comment, 'commentUser' => $this->commentUser, 'action' => $this->action);
 	}
 }
 class Log {
-	
+
 	private $log = array();
 	private $Count = -1;
 	private $appealID;
-	
+
 	public function __construct($vars) {
 		if ($vars) {
 			$this->appealID = $vars['appealID'];
 			$num_rows = mysql_num_rows($vars['dataset']);
-			
+
 			for ($i = 0; $i < $num_rows; $i++) {
 				//Creates a new log item with the data
 				$data = mysql_fetch_array($vars['dataset']);
@@ -51,25 +51,25 @@ class Log {
 			}
 		}
 	}
-	
+
 	public static function getCommentsByAppealId($id) {
 		$db = connectToDB();
-		
+
 		$query = "SELECT * from comment WHERE appealID = " . $id . " ORDER BY timestamp ASC;";
-		
+
 		$result = mysql_query($query, $db);
-		
+
 		if(!$result){
 			$error = mysql_error($db);
 			throw new UTRSDatabaseException($error);
 		}
-		
+
 		return new Log(array('dataset' => $result, 'appealID' => $id));
 	}
-	
+
 	public function addNewItem($comment, $action = null) {
 		$db = connectToDB();
-		
+
 		if (isset($_SESSION['user']) && strlen($_SESSION['user']) != 0) {
 			$user = User::getUserByUsername($_SESSION['user']);
 			//I have to use two user ids here because the sql query requires null to be sent as a string.
@@ -79,76 +79,77 @@ class Log {
 			$firstuserid = "null";
 			$seconduserid = null;
 		}
-		
+
 		if (!$action) {
 			$action = 0;
 		}
-		
+
 		$comment = sanitizeText(mysql_real_escape_string($comment));
-		
+
 		$timestamp = time();
-		
+
 		$query = "INSERT INTO comment (appealID, timestamp, comment, commentUser, action) VALUES (";
 		$query .= $this->appealID . ", ";
 		$query .= "NOW() , '";
 		$query .= $comment . "', ";
 		$query .= $firstuserid . ", ";
 		$query .= $action . ");";
-		
-		
+
+
 		$result = mysql_query($query, $db);
-		
+
 		if(!$result){
 			$error = mysql_error($db);
 			throw new UTRSDatabaseException($error);
 		}
-		
+
 		$id = mysql_insert_id($db);
-		
-		Appeal::getAppealById($this->appealID)->updateLastLogId($id);
-		
+
+		//Only update for actual actions
+		if ($action == 1) {
+			Appeal::getAppealById($this->appealID)->updateLastLogId($id);
+		}
+
 		$this->log[$this->Count + 1] = new LogItem(array('commentID' => $id, 'appealID' => $this->appealID, 'timestamp' => $timestamp, 'comment' => $comment, 'commentUser' => $seconduserid, 'action' => $action));
 		$this->Count++;
 	}
-	
+
 	function addAppellantReply($reply){
 		$db = connectToDB();
-		
+
 		$reply = sanitizeText(mysql_real_escape_string($reply));
-		
+
 		$timestamp = time();
-		
+
 		$query = "INSERT INTO comment (appealID, timestamp, comment, commentUser) VALUES (";
 		$query .= $this->appealID . ", ";
 		$query .= "NOW() , '";
 		$query .= $reply . "', ";
 		$query .= "NULL);";
-		
+
 		$result = mysql_query($query, $db);
-		
+
 		if(!$result){
 			$error = mysql_error($db);
 			throw new UTRSDatabaseException($error);
 		}
-		
+
 		$id = mysql_insert_id($db);
-		
-		Appeal::getAppealById($this->appealID)->updateLastLogId($id);
-		
+
 		$this->log[$this->Count + 1] = new LogItem(array('commentID' => $id, 'appealID' => $this->appealID, 'timestamp' => $timestamp, 'comment' => $reply, 'commentUser' => null, 'action' => null));
 		$this->Count++;
 	}
-	
+
 	public function getSmallHTML() {
-		
+
 		$HTMLOutput = "";
-		
+
 		$HTMLOutput .= "<table class=\"logTable\">";
 		$HTMLOutput .= "<tr>";
 		$HTMLOutput .= "<th class=\"logUserHeader\">User</th>";
 		$HTMLOutput .= "<th class=\"logActionHeader\">Action</th>";
 		$HTMLOutput .= "</tr>";
-		
+
 		for ($i = 0; $i < count($this->log); $i++) {
 			$styleUser = ($i%2 == 1) ? "smallLogUserOne" : "smallLogUserTwo";
 			$styleAction = ($i%2 == 1) ? "smallLogActionOne" : "smallLogActionTwo";
@@ -171,23 +172,23 @@ class Log {
 			$HTMLOutput .= "<td class=\"" . $styleAction . "\">" . $italicsStart . substr(sanitizeText(str_replace("\\r\\n", " ", $data['comment'])),0,150) . $italicsEnd . $dots . "</td>";
 			$HTMLOutput .= "</tr>";
 		}
-		
+
 		$HTMLOutput .= "</table>";
-		
+
 		return $HTMLOutput;
 	}
-	
+
 	public function getLargeHTML() {
-	
+
 		$HTMLOutput = "";
-	
+
 		$HTMLOutput .= "<table class=\"logLargeTable\">";
 		$HTMLOutput .= "<tr>";
 		$HTMLOutput .= "<th class=\"logLargeUserHeader\">User</th>";
 		$HTMLOutput .= "<th class=\"logLargeActionHeader\">Action</th>";
 		$HTMLOutput .= "<th class=\"logLargeTimeHeader\">Timestamp</th>";
 		$HTMLOutput .= "</tr>";
-	
+
 		for ($i = 0; $i < count($this->log); $i++) {
 			$styleUser = ($i%2 == 1) ? "largeLogUserOne" : "largeLogUserTwo";
 			$styleAction = ($i%2 == 1) ? "largeLogActionOne" : "largeLogActionTwo";
@@ -206,29 +207,29 @@ class Log {
 			$comment = $data['comment'];
 			// add links to other appeals: UTRS-67
 			$comment = preg_replace("~\[\[(\d+)\]\]~", '<a href="' . getRootURL() . 'appeal.php?id=$1">$1</a>', $comment);
-		
+
 			$HTMLOutput .= "<tr>";
 			$HTMLOutput .= "<td valign=top class=\"" . $styleUser . "\">" . $username . "</td>";
 			$HTMLOutput .= "<td valign=top class=\"" . $styleAction . "\">" . $italicsStart . sanitizeText(str_replace("\\r\\n", "<br>", $comment)) . $italicsEnd . "</td>";
 			$HTMLOutput .= "<td valign=top class=\"" . $styleTime . "\">" . $timestamp . "</td>";
 			$HTMLOutput .= "</tr>";
 		}
-	
+
 		$HTMLOutput .= "</table>";
-	
+
 		return $HTMLOutput;
 	}
-	
+
 	//IRC bot notification system
 	public static function ircNotification($message, $notify_unblock = 0) {
 		$db = connectToDB();
-				
+
 		$query = "INSERT INTO irc (notification, unblock) VALUES ('";
 		$query .= mysql_real_escape_string($message) . "', ";
 		$query .= $notify_unblock . ");";
-		
+
 		$result = mysql_query($query, $db);
-		
+
 		if(!$result){
 			$error = mysql_error($db);
 			throw new UTRSDatabaseException($error);

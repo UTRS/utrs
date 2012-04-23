74a66bd6e4ce2c6f475e81982c16100856bd4c46
Chris Howie <me@chrishowie.com>
2012-04-22 23:46:42 -0400

    Temporary include path fix
    The require_once('config.inc.php') call in unblocklib.php was grabbing the file
    from . first, which is $HOME for cron-invoked jobs, and we have a separate
    config.inc.php there.

--- public_html/src/unblocklib.php
+++ public_html/src/unblocklib.php
@@ -5,7 +5,7 @@ ini_set('session.use_cookies', '1');
 
 require_once('exceptions.php');
 require_once('userObject.php');
-require_once('src/config.inc.php');
+require_once(dirname(__FILE__) . '/config.inc.php');
 
 $GLOBALS['CHECKUSER'] = -1;
 $GLOBALS['APPROVED'] = 0;

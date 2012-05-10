UTRS/utrs

commit f84e8af6cbfa71b3ae68cd351f45341db784835d

Let Devs change CU flags

public_html/userMgmt.php
@@ -45,10 +45,9 @@ if(isset($_GET['userId']) & isset($_POST['submit']) & verifyAccess($GLOBALS['ADM
 					        "has been deactivated.");
 		}
 		// check credentials (unlikely someone will spoof the POST header, but just in case)
-		if(($newCheckuser != $checkuser) && (!$user->isCheckuser() || !$user->isDeveloper())){
+		if((($newCheckuser != $checkuser) || ($newCheckuser != $developer)) && (!$user->isCheckuser() || !$user->isDeveloper())){
 			throw new UTRSIllegalModificationException("You lack sufficient permission to make these " .
-					        "changes. The checkuser flag may only be changed by developers who also have the " .
-					        "checkuser flag.");
+					        "changes. The checkuser flag may only be changed by developers or checkusers. ");
 		}
 		if(($newDeveloper != $developer) & !$user->isDeveloper()){
 			throw new UTRSIllegalModificationException("You lack sufficient permission to make these " .
@@ -255,7 +254,7 @@ echo "<tr><td><label name=\"developerLabel\" id=\"developerLabel\" for=\"develop
      ($user->isDeveloper() ? "" : "readonly=\"readonly\" disabled=\"disabled\"") . " />\n</td></tr>";
 echo "<tr><td><label name=\"checkuserLabel\" id=\"checkuserLabel\" for=\"checkuser\">Checkuser:</label> </td><td>&#09;&#09; " .
      "<input name=\"checkuser\" id=\"checkuser\" type=\"checkbox\" " . ($checkuser ? "checked=\"checked\" " : " " ) . 
-     ($user->isDeveloper() & $user->isCheckuser() ? "" : " onClick=\"return false;\" ") . " />\n</td></tr></table>";
+     ($user->isDeveloper() | $user->isCheckuser() ? "" : " onClick=\"return false;\" ") . " />\n</td></tr></table>";
 echo "<input type=\"submit\" name=\"submit\" id=\"submit\" value=\"Submit changes\" \> ";
 echo "<input type=\"reset\" name=\"reset\" id=\"reset\" value=\"Reset\" onclick=\"setRequired(" . !$active . ")\" \>\n";
 echo "</form>\n";

commit bd31965fbb169134527b7492452da86d8f1a6b5c

fix bug caused by f84e8af6cb/46a1f70080

public_html/userMgmt.php
@@ -45,7 +45,7 @@ if(isset($_GET['userId']) & isset($_POST['submit']) & verifyAccess($GLOBALS['ADM
 					        "has been deactivated.");
 		}
 		// check credentials (unlikely someone will spoof the POST header, but just in case)
-		if((($newCheckuser != $checkuser) || ($newCheckuser != $developer)) && (!$user->isCheckuser() || !$user->isDeveloper())){
+		if(($newCheckuser != $checkuser) && (!$user->isCheckuser() && !$user->isDeveloper())){
 			throw new UTRSIllegalModificationException("You lack sufficient permission to make these " .
 					        "changes. The checkuser flag may only be changed by developers or checkusers. ");
 		}
6835668242ee7cfe6021de1536f3b8fd95c669b0
dqwiki <deltaquadwiki@gmail.com>
2012-06-06 12:31:02 -0400

    cleaner code

--- public_html/appeal.php
+++ public_html/appeal.php
@@ -307,7 +307,7 @@ if (isset($_GET['action'])) {
 	}
 }
 
-if (!($appeal->getStatus() == Appeal::$STATUS_UNVERIFIED)) {
+if ($appeal->getStatus() != Appeal::$STATUS_UNVERIFIED) {
 ?>
 <h1>Details for Request #<?php echo $appeal->getID(); ?>: <a href="<?php echo getWikiLink($appeal->getUserPage(), $user->getUseSecure()); ?>" target="_blank"><?php echo $appeal->getCommonName(); ?></a> :: ******<?php echo substr($appeal->getEmail(), strpos($appeal->getEmail(), "@")); ?></h1>
 <table class="appeal">

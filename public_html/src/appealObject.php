be19262be3905640a850ef93f8552f526d201938
DeltaQuad <deltaquadwiki@gmail.com>
2015-02-21 12:48:50 -0500

    debug

--- public_html/src/appealObject.php
+++ public_html/src/appealObject.php
@@ -726,6 +726,7 @@ class Appeal extends Model {
         $review = count(preg_match("^.*\{\{(U|u)nblock.*reviewed^",strtolower($data)));
         $unblock = count(preg_match("^.*\{\{(U|u)nblock^",$data)); 
         if ($review<$unblock) {
+          throw new UTRSValidationException("Review count: ".$review.", Unblock count:".$unblock);
           return False;
         }
       }

570c948c1ee14e245392a9c9b8698ea69ee99700
DeltaQuad <deltaquadwiki@gmail.com>
2015-02-21 00:44:29 -0500

    Force clearing

--- public_html/src/config.inc.php
+++ public_html/src/config.inc.php
@@ -1,15 +1,7 @@
 <?php
-<<<<<<< HEAD
-
 $raw = @file_get_contents(dirname(__FILE__) . '/config.js.php');
 $file = str_replace("<?php","",$raw);
 $CONFIG = json_decode($file, true);
-
-=======
-$raw = @file_get_contents(dirname(__FILE__) . '/config.js.php');
-$file = str_replace("<?php","",$raw);
-$CONFIG = json_decode($file, true);
->>>>>>> origin/deltaquad-appeal-validation
 if (json_last_error() != JSON_ERROR_NONE || is_null($CONFIG)) {
     trigger_error('config.js.php does not exist or contains invalid JSON.', E_USER_ERROR);
 }

f3ce53cf6c5f7e382b43bc5af2cb912641e7eb97
DeltaQuad <deltaquadwiki@gmail.com>
2015-02-20 22:56:54 -0500

    update config file location

--- public_html/src/config.inc.php
+++ public_html/src/config.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-$CONFIG = json_decode(@file_get_contents(dirname(__FILE__) . '/config.js'), true);
+$CONFIG = json_decode(@file_get_contents(dirname(__FILE__) . '/config.js.php'), true);
 
 if (json_last_error() != JSON_ERROR_NONE || is_null($CONFIG)) {
-    trigger_error('config.js does not exist or contains invalid JSON.', E_USER_ERROR);
+    trigger_error('config.js.php does not exist or contains invalid JSON.', E_USER_ERROR);
 }
 
 ?>

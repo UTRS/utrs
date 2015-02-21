7467320df50d664fbb22f4a42bd5e5021b9c6af5
DeltaQuad <deltaquadwiki@gmail.com>
2015-02-20 23:36:49 -0500

    fix

--- public_html/src/appealObject.php
+++ public_html/src/appealObject.php
@@ -704,7 +704,7 @@ class Appeal extends Model {
    }
    public function verifyBlock($username) {
       $data = json_decode(file_get_contents('http://en.wikipedia.org/w/api.php?action=query&list=users&ususers='.$username.'&format=json&usprop=blockinfo'));
-      echo $data
+      echo $data;
       try{
         var_dump($data["users"]["blockid"]);
       } catch (Exception $e) {

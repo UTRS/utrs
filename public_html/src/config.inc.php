75675a261ca2e3206423752482194ea75ba7bd6e
DeltaQuad <deltaquadwiki@gmail.com>
2015-02-20 23:29:41 -0500

    fix config file per beta/live

--- public_html/src/config.inc.php
+++ public_html/src/config.inc.php
@@ -1,9 +1,8 @@
 <?php
-
-$CONFIG = json_decode(@file_get_contents(dirname(__FILE__) . '/config.js'), true);
-
+$raw = @file_get_contents(dirname(__FILE__) . '/config.js.php');
+$file = str_replace("<?php","",$raw);
+$CONFIG = json_decode($file, true);
 if (json_last_error() != JSON_ERROR_NONE || is_null($CONFIG)) {
-    trigger_error('config.js does not exist or contains invalid JSON.', E_USER_ERROR);
+    trigger_error('config.js.php does not exist or contains invalid JSON.', E_USER_ERROR);
 }
-
 ?>

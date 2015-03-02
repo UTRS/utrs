f9454e171ff87011509a3a0881c24587ea69e961
DeltaQuad <deltaquadwiki@gmail.com>
2015-03-02 16:51:03 -0500

    wrong logic

--- public_html/appeal.php
+++ public_html/appeal.php
@@ -275,7 +275,7 @@ if (isset($_GET['action']) && isset($_GET['value']) && $_GET['action'] == "statu
 				//admin
 				verifyAccess($GLOBALS['ADMIN']) &&
 				//When assigned
-				($appeal->getHandlingAdmin() != NULL)
+				($appeal->getHandlingAdmin() === NULL)
 				) {
 				$appeal->setStatus(Appeal::$STATUS_NEW);
 				$log->addNewItem('Reset appeal to NEW', 1);

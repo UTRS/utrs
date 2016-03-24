425d564ac57ac264e1dd0a5cb44c7d1e3394db33
DeltaQuad <deltaquadwiki@gmail.com>
2016-03-24 08:09:02 -0400

    pass on vars

--- public_html/appeal.php
+++ public_html/appeal.php
@@ -499,7 +499,7 @@ Status: <b><?php echo $appeal->getStatus(); ?></b><br>
 </ul>
 </div>
 <?php }
-function cuwindow() {
+function cuwindow($appeal,$user) {
 	if ($appeal->checkRevealLog($user->getUserId(), "cudata")) {
 		echo htmlspecialchars(json_encode(nl2br($appeal->getIP() . " " . $appeal->getUserAgent())));
 	}
@@ -509,7 +509,7 @@ function cuwindow() {
 }
 if (verifyAccess($GLOBALS['CHECKUSER']) || verifyAccess($GLOBALS['WMF'])) {
 	?>
-<h3><a href="javascript:void(0)" onClick="showContextWindow(<?php cuwindow() ?>)">User Agent</a></h3>
+<h3><a href="javascript:void(0)" onClick="showContextWindow(<?php cuwindow($appeal,$user) ?>)">User Agent</a></h3>
 <div class="info" style="height:60px !important;"><?php 
 if ($appeal->checkRevealLog($user->getUserId(), "cudata")) {
 		echo $appeal->getIP() . " " . $appeal->getUserAgent();

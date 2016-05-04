1e8a414489403b83fc0961aec9674dd3a3c958db
DeltaQuad <deltaquadwiki@gmail.com>
2016-05-04 01:22:11 -0400

    upgrade jquery

--- public_html/template.php
+++ public_html/template.php
@@ -357,8 +357,7 @@ For questions or assistance with the Unblock Ticket Request System, please email
 Version <?php echo getVersion() ?>.</p>
 </div>
 <!-- ACC Claims that putting these at the bottom decreases load time. Plus do we need both jquerys? Plus are they not out of date? --DQ -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.js" type="text/javascript"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
 <script src="js/bootstrap.min.js"></script>
 </body>
 </html>

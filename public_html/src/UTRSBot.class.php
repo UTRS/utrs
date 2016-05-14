aa826c45675a4aa80990c15ddaf311ae885ff87f
TParis <tparis@ufpfleet.org>
2016-05-14 13:09:35 -1000

    Add in functionality to UTRSBot to close appeals on-wiki
    Closes appeals on-wiki after they have been closed on UTRS.

--- public_html/src/UTRSBot.class.php
+++ public_html/src/UTRSBot.class.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once(__DIR__ . "/../includes/Peachy/Init.php");
+require_once(__DIR__ . "/../src/unblocklib.php");
 
 class UTRSBot {
    
@@ -11,6 +12,7 @@ class UTRSBot {
    private $adminTemplate		= "UTRS-unblock-admin";
    private $oppTemplate			= "UTRS-OPP";
    private $oppPage				= "Wikipedia:WikiProject_on_open_proxies/Requests";
+   private $userTempPtrn		= "/\{\{UTRS-unblock-user\|(\d{0,5})?\|([a-zA-Z]{1,4} [0-9]{1,2}, [0-9]{1,4} [0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2})?\}\}/g";
       
    public function __construct() {
       
@@ -111,6 +113,61 @@ class UTRSBot {
 			)
 	   );
 	   
-	   print_r($result);
+	   //Filter out other nonsense
+	   $results = $results["query"]["categorymembers"];
+	   
+	   foreach ($results as $userPage) {
+		   
+		   //Get vars $page, $user
+		   $page = $this->objPeachy->initPage( $userPage["title"] );
+		   $user = split(":", $userPage["title"]);
+		   $user = $user[1];
+		   
+		   echo "Reviewing " . $user;
+		   
+		   //Get Page text
+		   $text = $page->get_text();
+		   
+		   //Get specific template
+		   $matches = array();
+		   $found = preg_match($this->userTempPtrn, $text, $matches);
+		   
+		   if ($found === true) {
+			   
+			   echo "Open UTRS Unblock template found";
+			   
+			   //Find out if ticket is still open
+			   $db = connectToDB();
+			   
+			   //SQL injection protection
+			   $id = is_numeric($matches[1]) ? $matches[1] : 0;
+			   
+			   $query = $db->prepare("SELECT status FROM appeal WHERE appealID = :appealid;");
+			   
+			   $query->execute(array(":appealid" => $id));
+			   
+			   $row = $query->fetch(PDO::FETCH_ASSOC);
+	
+			   if ($row["status"] == "CLOSED") {
+				   
+				   echo "Appeal is closed";
+				   
+				   //Create new template
+				   $new_template = "{{UTRS-unblock-user|" . $matches[1] . "|" . $matches[2] . "|closed}}";
+				   
+				   //Replace template
+				   str_replace($matches[0], $new_template, $text);
+				   
+				   //Edit Page
+				   echo $text;
+				   //$page->edit($text);\
+				 
+				   echo "Page saved";
+				 
+			   } else {
+				   echo "Appeal is still open, moving on...";
+			   }
+		   }
+	   }
    }
 }

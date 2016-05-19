bdfc1b3e194652e07437d17f43324b432eefa6b5
DeltaQuad <deltaquadwiki@gmail.com>
2016-05-19 13:11:02 -0400

    ffs

--- public_html/src/checkuserDataRemoval.php
+++ public_html/src/checkuserDataRemoval.php
@@ -6,7 +6,7 @@
 require_once('unblocklib.php');
 require_once('exceptions.php');
 require_once(__DIR__ . "/../includes/Peachy/Init.php");
-require_once(__DIR__ . "/../src/unblocklib.php");
+require_once(__DIR__ . "/../src/appealObject.php");
 
 echo "Starting to clear out private data from closed appeals.\n";
 

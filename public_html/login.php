58e7eff15776fca98eae5f6922778d962a33f09e
DeltaQuad <deltaquadwiki@gmail.com>
2016-06-07 22:56:05 -0400

    bootstrap causing issues I don't want to deal with

--- public_html/login.php
+++ public_html/login.php
@@ -170,9 +170,7 @@ if (isset($_GET['lang'])) {
          <td colspan="2">&nbsp;</td>
       </tr>
    </table>
-   <?php //This is set to hidden as it will be the actual form selection that we are going to grab from the url ?>
-   <input id="lang" name="lang" value="<?php if(isset($lang)){echo $lang;} else{	echo "en"; } ?>" type="hidden">
-   <input class="label label-primary" id="login" name="login" value="Login" type="submit">
+   <input id="login" name="login" value="Login" type="submit">
 </form>
 <p>You must have cookies enabled in order to log in.</p>
 

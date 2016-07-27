603ff6e2e7a0c7328dbb69b83206cf7f1156e6d7
Amanda/DeltaQuad <deltaquadwiki@gmail.com>
2016-07-27 10:05:38 -0400

    Fix #99

--- public_html/banMgmt.php
+++ public_html/banMgmt.php
@@ -20,6 +20,8 @@ $errors = '';
 $target = null;
 $displayTarget = null;
 
+global $lang;
+
 try{
 	if(!verifyAccess($GLOBALS['ADMIN'])){
 		throw new UTRSCredentialsException(SystemMessages::$error['TooladminsOnlyBan'][$lang]);

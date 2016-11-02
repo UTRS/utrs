80048af621330f22a404fce273afa230d725cdc8
Amanda/DeltaQuad <deltaquadwiki@gmail.com>
2016-11-02 13:07:06 -0400

    try splashing

--- public_html/src/network.php
+++ public_html/src/network.php
@@ -1,5 +1,21 @@
 <?php
 
+
+$online = False;
+$expected = False;
+if (!$online) {
+  skinHeader();
+  echo "<center><h2>UTRS is down :(</h2>"
+  if ($expected) {
+    echo "<br />This is a scheduled maintence window in which UTRS is down. We hope to be live again as soon as possibe.</center>"
+  }
+  else {
+    echo "<br />This is a unscheduled maintence in which UTRS is down. UTRS has died on us and we are working to fix it as soon as possible. <br />We hope to be live again as soon as possibe.</center>"
+  }
+  skinFooter();
+  die();
+} 
+
 // Borrowed from ACC/Simon Walker: https://phabricator.stwalkerster.co.uk/P27
 
 function isHttps()

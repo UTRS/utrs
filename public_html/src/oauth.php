ac957b2c05d00a6e275606f86be451406431411c
Amanda/DeltaQuad <deltaquadwiki@gmail.com>
2017-01-30 20:58:21 -0500

    it's call back that's the issue

--- public_html/src/oauth.php
+++ public_html/src/oauth.php
@@ -75,7 +75,7 @@ function sign_request( $method, $url, $params = array() ) {
  * @return void
  */
 function doAuthorizationRedirect() {
-    global $mwOAuthUrl, $mwOAuthAuthorizeUrl, $gUserAgent, $gConsumerKey, $gTokenSecret, $CONFIG;
+    global $mwOAuthUrl, $mwOAuthAuthorizeUrl, $gUserAgent, $gConsumerKey, $gTokenSecret, $CONFIG, $wiki;
 
     // First, we need to fetch a request token.
     // The request is signed with an empty token secret and no token key.
@@ -86,7 +86,7 @@ function doAuthorizationRedirect() {
         'format' => 'json',
         
         // OAuth information
-        'oauth_callback' => $CONFIG['site_root'].'login.php', // Must be "oob" or something prefixed by the configured callback URL
+        'oauth_callback' => $CONFIG['site_root'].'login.php?wiki='.$wiki, // Must be "oob" or something prefixed by the configured callback URL
         'oauth_consumer_key' => $gConsumerKey,
         'oauth_version' => '1.0',
         'oauth_nonce' => md5( microtime() . mt_rand() ),

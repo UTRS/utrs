0aefa626a6938ff09f824ec8c20a8dc30e0f4b2c
Amanda/DeltaQuad <deltaquadwiki@gmail.com>
2017-02-05 23:30:51 -0500

    try this

--- public_html/login.php
+++ public_html/login.php
@@ -34,12 +34,7 @@ if(!empty($_GET)){
 	$round = 1;
 	$forwardString = "";
 	foreach ($_GET as $key => $value) {
-		if ($round===1) {
-			$forwardString .= $key . "=" . $value;
-		}
-		else {
-			$forwardString .= "&".$key . "=" . $value;
-		}
+		$forwardString .= "&".$key . "=" . $value;
 	}
 }
 else{
